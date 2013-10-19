17169ad8b13946a39f00c4b0525ecf5c14a2bbe5
sonnym <michaud.sonny@gmail.com>
2013-10-19 11:39:00 -0400

    supply user credentials to youtube_it

--- app/models/video_uploader.rb
+++ app/models/video_uploader.rb
@@ -1,4 +1,6 @@
 class VideoUploader
+  YOUTUBE_USERNAME = 'GourceTV'
+  YOUTUBE_PASSWORD = 'oiJOS9f238!nxjni'
   YOUTUBE_KEY = 'AIzaSyC_nu1u3daHzQPimuQ4c3-JHTtuzDv3Ymk'
 
   def initialize(repository)
@@ -6,7 +8,9 @@ class VideoUploader
   end
 
   def upload!
-    client = YouTubeIt::Client.new(:dev_key => YOUTUBE_KEY)
+    client = YouTubeIt::Client.new(dev_key: YOUTUBE_KEY,
+                                   username: YOUTUBE_USERNAME,
+                                   password: YOUTUBE_PASSWORD)
     client.video_upload(File.open(@repository.video_path), settings)
   end
 
