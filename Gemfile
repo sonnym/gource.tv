a04c33935c794d61180ef77400a172fae4eeca59
sonnym <michaud.sonny@gmail.com>
2013-10-18 21:39:54 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'https://rubygems.org'
+
+# core
+gem 'rails', '4.0.0'
+gem 'mysql2'
+
+# integration
+gem 'rugged'
+gem 'youtube_it'
+
+# jobs
+gem 'resque'
+
+# monitor
+gem 'peek'
+gem 'peek-resque'
+
+# assets
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+
+group :development do
+  gem 'capistrano', '~> 2.15'
+end
+
+group :development, :test do
+  gem 'pry'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gourcetv::Application.config.secret_key_base = 'decdd1332b227fa4ba80582e9ec5cf2f427477942eb0e62de036cfbe62775cd323170684ea39ea1680345675ea81fcebc9a50dcfbb4139d496b6d3912eef399c'
