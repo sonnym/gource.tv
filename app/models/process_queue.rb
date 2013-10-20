3dc0c025048f1f185607ea78501a9d7f8ce99c47
sonnym <michaud.sonny@gmail.com>
2013-10-20 16:42:56 -0400

    retry uploading to youtube or requeue

--- app/models/process_queue.rb
+++ app/models/process_queue.rb
@@ -3,17 +3,19 @@ class ProcessQueue
 
   def initialize(repository)
     @repository = repository
+    @retry = false
   end
 
   def process!
     with_log do
       with_repository do
         with_video do
-          response = VideoManager.new(@repository).upload!
-          @repository.update_attributes(youtube_id: response.unique_id, processing: false)
+          upload!(5)
         end
       end
     end
+
+    Resque.enqueue(ProcessQueue, @repository) if @retry
   end
 
   def self.perform(repository_hash)
@@ -27,7 +29,7 @@ class ProcessQueue
 
     yield
 
-    log.save
+    log.save unless @retry
   end
 
   def with_repository
@@ -44,5 +46,20 @@ class ProcessQueue
     yield
 
     File.delete(@repository.video_path)
+  end
+
+  def upload!(retries)
+    begin
+      response = VideoManager.new(@repository).upload!
+      @repository.update_attributes(youtube_id: response.unique_id, processing: false)
+    rescue
+      retries -= 1
+
+      if retries > 0
+        retry
+      else
+        @retry = true
+      end
+    end
   end
 end
