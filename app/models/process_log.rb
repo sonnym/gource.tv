5eece8e6e1a523542926c19abdebd90ae3659957
sonnym <michaud.sonny@gmail.com>
2013-10-20 13:23:51 -0400

    update process log display

--- app/models/process_log.rb
+++ app/models/process_log.rb
@@ -1,3 +1,17 @@
 class ProcessLog < ActiveRecord::Base
   belongs_to(:repository)
+
+  %i(started_at created_at updated_at).each do |attr|
+    define_method(attr) do
+      read_attribute(:started_at).to_s('%F %T')
+    end
+  end
+
+  def duration
+    read_attribute(:created_at).to_time - read_attribute(:started_at).to_time
+  end
+
+  def as_json(opts = {})
+    super(opts.merge(methods: %i(duration)))
+  end
 end
