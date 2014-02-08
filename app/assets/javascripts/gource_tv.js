ebb3fcd187289b3dc6aa7dfd44749a498776e4f1
sonnym <michaud.sonny@gmail.com>
2014-02-08 13:52:44 -0500

    fix sce issues

--- app/assets/javascripts/gource_tv.js
+++ app/assets/javascripts/gource_tv.js
@@ -1,5 +1,12 @@
 var gourceTV = angular.module("gourceTV", ["ngResource", "ui.bootstrap"]);
 
+gourceTV.config(["$sceDelegateProvider", function($sceDelegateProvider) {
+  $sceDelegateProvider.resourceUrlWhitelist([
+    "self",
+    "http://www.youtube.com/embed/*?rel=0&vq=hd720&wmode=transparent"
+  ]);
+}]);
+
 gourceTV.run(["$rootScope", function($rootScope) {
   $rootScope.goHome = function() {
     $rootScope.$broadcast("repository:clear");
