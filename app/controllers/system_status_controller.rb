5e4c36b19d6f7af30f8e603437a5569115f025c4
sonnym <michaud.sonny@gmail.com>
2014-02-08 15:48:35 -0500

    display offline status when redis is down

--- app/controllers/system_status_controller.rb
+++ app/controllers/system_status_controller.rb
@@ -1,5 +1,15 @@
 class SystemStatusController < ApplicationController
   def index
-    render(json: { core: SystemStatus.get, resque: Resque.info })
+    render(json: { core: SystemStatus.get, resque: resque_info })
+  end
+
+  private
+
+  def resque_info
+    begin
+      Resque.info
+    rescue
+      { offline: true }
+    end
   end
 end
