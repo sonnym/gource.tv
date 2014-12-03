aab02f350ed2223e7b2438d53e8731da536af062
sonnym <michaud.sonny@gmail.com>
2014-12-03 14:57:10 -0500

    update exception notification configuration

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -2,15 +2,17 @@ require 'exception_notification/rails'
 
 require 'resque/failure/multiple'
 require 'resque/failure/redis'
-
 require 'exception_notification/resque'
 
 Resque::Failure::Multiple.classes = [Resque::Failure::Redis, ExceptionNotification::Resque]
 Resque::Failure.backend = Resque::Failure::Multiple
 
-Gourcetv::Application.config.middleware.use(ExceptionNotification::Rack,
-  email: {
+ExceptionNotification.configure do |config|
+  config.add_notifier :email, {
     email_prefix: "[#{Rails.env}] ",
     sender_address: %{"notifier" <notifier@gource.tv>},
     exception_recipients: %w{gourcetv+exceptions@gmail.com}
- })
+  }
+end
+
+Gourcetv::Application.config.middleware.use(ExceptionNotification::Rack)
