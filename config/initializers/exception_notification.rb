7cc706886528156b9023214cd88086dd2939e9c8
sonnym <michaud.sonny@gmail.com>
2014-12-03 13:19:54 -0500

    configure resque to send exception notifications

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,6 +1,16 @@
+require 'exception_notification/rails'
+
+require 'resque/failure/multiple'
+require 'resque/failure/redis'
+
+require 'exception_notification/resque'
+
+Resque::Failure::Multiple.classes = [Resque::Failure::Redis, ExceptionNotification::Resque]
+Resque::Failure.backend = Resque::Failure::Multiple
+
 Gourcetv::Application.config.middleware.use(ExceptionNotification::Rack,
-  :email => {
-    :email_prefix => "[#{Rails.env}] ",
-    :sender_address => %{"notifier" <notifier@gource.tv>},
-    :exception_recipients => %w{gourcetv+exceptions@gmail.com}
-  })
+  email: {
+    email_prefix: "[#{Rails.env}] ",
+    sender_address: %{"notifier" <notifier@gource.tv>},
+    exception_recipients: %w{gourcetv+exceptions@gmail.com}
+ })
