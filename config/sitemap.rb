5b361059b25c3fc70f81ff8ca9108f7953279ce7
sonnym <michaud.sonny@gmail.com>
2015-01-13 16:16:06 -0500

    set sitemap urls to change daily

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -2,6 +2,6 @@ SitemapGenerator::Sitemap.default_host = "http://gource.tv/"
 
 SitemapGenerator::Sitemap.create do
   Repository.all.each do |repository|
-    add repository_path(repository), lastmod: repository.updated_at, changefreq: :yearly
+    add repository_path(repository), lastmod: repository.updated_at, changefreq: :daily
   end
 end
